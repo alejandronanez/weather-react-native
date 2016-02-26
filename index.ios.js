08fbdc763fb8221ac4b379e4aac66c818d73ec1e
Alejandro Nanez <alejonanez@gmail.com>
2016-02-25 21:22:18 -0500

    Adding a pin to the map

--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,35 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
 import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    MapView,
+    View,
+    StyleSheet
 } from 'react-native';
 
-class weather extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+const Weather = React.createClass({
+    getInitialState() {
+        return {
+            pins: []
+        }
+    },
+    onRegionChangeComplete({longitude, latitude}) {
+        const pins = this.state.pins.concat({longitude, latitude});
+        this.setState({pins});
+    },
+    render() {
+        return (
+            <MapView
+                style={styles.map}
+                annotations={this.state.pins}
+                onRegionChangeComplete={this.onRegionChangeComplete}>
+            </MapView>
+        );
+    }
 });
 
-AppRegistry.registerComponent('weather', () => weather);
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    }
+});
+
+AppRegistry.registerComponent('weather', () => Weather);
